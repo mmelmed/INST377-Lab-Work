599c938e733ee0c57587076812d1186453fe5ccf
mmelmed <mattmelmed@gmail.com>
2022-04-08 16:36:06 -0400

    finished lab 4

---
+++ client/lab_4/script.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable-next-line no-restricted-syntax */
+
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel_item');
+const totalSlides = slides.length;
+
+function updateSlidePosition() {
+  for (const slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
+}
+
+document.querySelector('#carousel_button-prev').addEventListener('click', () => { moveToPrevSlide(); });
+document.querySelector('#carousel_button-next').addEventListener('click', () => { moveToNextSlide(); });
